236196c68bdf14a99e50c11df56eacf64872717f
Kim-Ju-Yeop <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-01-02 16:49:40 +0900

    [ADD] (1) Kotlin 예제 - val, var 키워드 사용
    val, var 키워드를 사용하여 간단하게 구성해본 예제입니다.

---
+++ Example/(1) Kotlin 예제 - val, var 키워드 사용.kt
@@ -0,0 +1,13 @@
+package chap02.section2
+
+// val, var 키워드를 이용하여 실습해본 예제입니다.
+fun main() {
+
+    val number = 10
+    var language = "Korean"
+
+    language = "English"
+
+    println("number : $number")
+    println("language : $language")
+}
