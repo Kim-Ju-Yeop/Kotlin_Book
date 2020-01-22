f9c6967e9977a8e07df982d59dd120c2587ca7bb
Kim-Ju-Yeop <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-01-22 22:19:00 +0900

    [ADD] Kotlin 예제 [27] ~ [29]

---
+++ Example/[29] Kotlin 예제 - return 문으로 람다식 탈출하기.kt
@@ -0,0 +1,14 @@
+package chap03.section5
+
+fun main() {
+
+    shortFunc3(3){
+        println("First call : $it")
+        return}
+}
+
+inline fun shortFunc3(a : Int, out : (Int) -> Unit){
+    println("Before calling out()")
+    out(a)
+    println("After calling out()")
+}
